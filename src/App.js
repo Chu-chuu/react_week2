e229734e4954ffbc7a892dd07ad2d4a0d45e52c6
Ngozi Umekwe <umekweochuwa@gmail.com>
2023-08-29 12:28:36 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+// let img = ['https://images.unsplash.com/photo-1691836691308-868a5db822c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80',];
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Creative Writing Through Visual Inspiration</h1>
+        <h3>Introduction</h3>
+        <p>In this class exercise, we invite you to embark on a journey of creative writing inspired by a captivating image. The image features a girl's face gracefully nestled among vibrant flowers, evoking a sense of beauty, tranquility, and wonder. This exercise aims to unleash your imagination, encouraging you to craft descriptive paragraphs that bring the scene to life through words.</p>
+        <img src="https://images.unsplash.com/photo-1691836691308-868a5db822c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" alt="" width={"60%"} height={"300rem"} />
+        <h3>Image Description</h3>
+        <p>Take a moment to observe the image closely. The girl's serene expression exudes a sense of calm as she becomes one with the surrounding flowers. The delicate petals frame her face like a colorful halo, while the sunlight gently illuminates her features. The vivid hues of the blossoms contrast with the softness of her skin, creating a captivating visual contrast.</p>
       </header>
     </div>
   );
